def703e1f84ba6ce8db96dacdc894cabcc7e6a21
Mahesh <theonetech@Practical.local>
2019-06-13 19:32:19 +0530

    Implemented multiple file selection

--- app/src/main/java/com/mahesh/documentpicker/MainActivity.java
+++ app/src/main/java/com/mahesh/documentpicker/MainActivity.java
@@ -5,27 +5,44 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.mahesh.documentpicker2.DocumentPickerClass;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private int RequestCode = 1;
-    File file;
-
+    ArrayList<File> file = new ArrayList<>();
+    Button openImagePicker;
+    TextView tvFileCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DocumentPickerClass.browseDocuments(this, RequestCode);
+        tvFileCount = (TextView) findViewById(R.id.tvFileCount);
+        openImagePicker = (Button) findViewById(R.id.btnOptnImagePicker);
+        openImagePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImagePicker();
+            }
+        });
+    }
+
+    public void openImagePicker() {
+        DocumentPickerClass.browseDocuments(this, RequestCode, true);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         file = DocumentPickerClass.onActivityResult(this, requestCode, resultCode, data);
+        tvFileCount.setText("Selected file count " + file.size());
     }
 
     @Override
--- documentpicker2/src/main/java/com/mahesh/documentpicker2/DocumentPickerClass.java
+++ documentpicker2/src/main/java/com/mahesh/documentpicker2/DocumentPickerClass.java
@@ -3,6 +3,7 @@ package com.mahesh.documentpicker2;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
@@ -22,6 +23,7 @@ import android.webkit.MimeTypeMap;
 import android.widget.Toast;
 
 import java.io.File;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
@@ -30,6 +32,8 @@ public class DocumentPickerClass {
     private static final int PERMISSION_ALL = 1;
     private static int DOCUMENT_PICKER_CODE = 0;
     private static File file;
+    private static ArrayList<File> fileList = new ArrayList<>();
+    private static boolean allowsMultiple = false;
 
     static String[] PERMISSIONS = {
             android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -37,9 +41,11 @@ public class DocumentPickerClass {
     };
 
 
-    public static void browseDocuments(Activity context, int requestCode) {
+    public static void browseDocuments(Activity context, int requestCode, boolean allowMultiple) {
+        fileList = new ArrayList<>();
         hasPermissions(context, PERMISSIONS);
         DOCUMENT_PICKER_CODE = requestCode;
+        allowsMultiple = allowMultiple;
         if (!hasPermissions(context, PERMISSIONS)) {
             ActivityCompat.requestPermissions(context, PERMISSIONS, PERMISSION_ALL);
         } else {
@@ -48,7 +54,7 @@ public class DocumentPickerClass {
             intent.setType("*/*");
             String[] mimetypes = {"image/*", "application/pdf", "video/*"};
             intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
-            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false);
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, allowsMultiple);
             intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
             context.startActivityForResult(Intent.createChooser(intent, "ChooseFile"), requestCode);
         }
@@ -124,10 +130,16 @@ public class DocumentPickerClass {
     }
 
 
-    public static File onActivityResult(Context context, int requestCode, int resultCode, Intent data) {
+    public static ArrayList<File> onActivityResult(Context context, int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK && requestCode == DOCUMENT_PICKER_CODE) {
             try {
-                return getFile(data.getData(), context);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    if (data.getClipData() != null) {
+                        return getFileList(data.getClipData(), context);
+                    } else {
+                        return getFile(data.getData(), context);
+                    }
+                }
             } catch (URISyntaxException e) {
                 e.printStackTrace();
             }
@@ -135,8 +147,66 @@ public class DocumentPickerClass {
         return null;
     }
 
+    private static ArrayList<File> getFileList(ClipData data, Context context) throws URISyntaxException {
+        ClipData mClipData = data;
+        ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+        for (int i = 0; i < mClipData.getItemCount(); i++) {
+            ClipData.Item item = mClipData.getItemAt(i);
+            Uri uri = item.getUri();
+            String path = getPath(context, uri);
+            ArrayList<String> video_extentions = new ArrayList<>();
+            video_extentions.add("3g2");
+            video_extentions.add("mp4");
+            video_extentions.add("mkv");
+            video_extentions.add("3gp");
+            video_extentions.add("asf");
+            video_extentions.add("avi");
+            video_extentions.add("flv");
+            video_extentions.add("m4v");
+            video_extentions.add("mov");
+            video_extentions.add("mpg");
+            video_extentions.add("rm");
+            video_extentions.add("srt");
+            video_extentions.add("swf");
+            video_extentions.add("vob");
+            video_extentions.add("wmv");
+            video_extentions.add("pdf");
+            video_extentions.add("jpg");
+            video_extentions.add("png");
+            video_extentions.add("jpeg");
 
-    private static File getFile(Uri data, Context context) throws URISyntaxException {
+            if (path != null) {
+                file = new File(path);
+                int extensionDelimiter = file.getPath().lastIndexOf(".");
+                String extension1 = "";
+                if (extensionDelimiter != -1) {
+                    extension1 = file.getPath().substring(extensionDelimiter + 1, file.getPath().length());
+                }
+
+                //max size 10MB Modified by mahesh 150319
+                if (file != null && file.length() > 0) {
+                    int file_size = Integer.parseInt(String.valueOf(file.length() / 1024));
+                    if (file_size > 10240) {
+                        Toast.makeText(context, "The file you are trying to send is too large.Please modify it before sending.", Toast.LENGTH_SHORT).show();
+                        return null;
+                    }
+                }
+
+                if (extension1 != null && !extension1.equals("") && video_extentions.contains(extension1)) {
+                    fileList.add(file);
+                    if (i == mClipData.getItemCount() - 1) {
+                        return fileList;
+                    }
+                } else {
+                    Toast.makeText(context, "File Formate not supported.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        return null;
+    }
+
+
+    private static ArrayList<File> getFile(Uri data, Context context) throws URISyntaxException {
         String path = getPath(context, data);
         ArrayList<String> video_extentions = new ArrayList<>();
         video_extentions.add("3g2");
@@ -177,7 +247,8 @@ public class DocumentPickerClass {
             }
 
             if (extension1 != null && !extension1.equals("") && video_extentions.contains(extension1)) {
-                return file;
+                fileList.add(file);
+                return fileList;
             } else {
                 Toast.makeText(context, "File Formate not supported.", Toast.LENGTH_LONG).show();
             }
@@ -204,7 +275,7 @@ public class DocumentPickerClass {
             intent.setType("*/*");
             String[] mimetypes = {"image/*", "application/pdf", "video/*"};
             intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
-            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false);
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, allowsMultiple);
             intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
             context.startActivityForResult(Intent.createChooser(intent, "ChooseFile"), DOCUMENT_PICKER_CODE);
         }
